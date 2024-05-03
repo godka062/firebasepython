f9fec64ad4c99dcaa6e43ed3b1d3c79a7989e68d
godka062 <104710977+godka062@users.noreply.github.com>
2024-05-03 21:16:32 +0900

    Update firebasepython.py

--- firebasepython.py
+++ firebasepython.py
@@ -1,55 +1,49 @@
 import streamlit as st
-from firebase_admin import credentials, db, initialize_app, get_app
 from datetime import datetime, timedelta
 from statistics import stdev
 
-# Firebase 인증 및 초기화
+# Firebase 연동
+import firebase_admin
+from firebase_admin import credentials, db
+
+# Firebase 인증 정보 로드
 cred = credentials.Certificate("C:/test/test-486a8-firebase-adminsdk-6jl9k-2bf67a04df.json")
-try:
-    app = get_app()
-except ValueError:
-    app = initialize_app(cred, {'databaseURL': 'https://test-486a8-default-rtdb.firebaseio.com/'})
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://test-486a8-default-rtdb.firebaseio.com/'})
 
-ref = db.reference('realPower')
-
-# 사용자가 선택한 업데이트 간격을 가져옴
-update_interval = st.slider("데이터 업데이트 간격 (초)", min_value=1, max_value=60, value=10, key='interval_slider')
-st.write(f"데이터는 매 {update_interval} 초마다 업데이트됩니다.")
-
+# Firebase에서 데이터 가져오는 함수
 def fetch_data():
+    ref = db.reference('realPower')
     data = ref.get()
     if data:
         return [(datetime.strptime(key, "%Y-%m-%d %H:%M:%S"), value) for key, value in data.items()]
     else:
         return []
 
+# 실시간 전력 모니터링
+st.title("실시간 전력 모니터링")
+
+# 데이터 업데이트 간격 설정
+update_interval = st.slider("데이터 업데이트 간격 (초)", min_value=1, max_value=60, value=10, key='interval_slider')
+st.write(f"데이터는 매 {update_interval} 초마다 업데이트됩니다.")
+
+# 데이터 가져오기
+data = fetch_data()
+
+# 최근 데이터 가져오기
 def get_recent_data(data):
     if len(data) > 20:
         return data[-20:]
     else:
         return data
 
-st.title("실시간 전력 모니터링")
-
-data = fetch_data()
-
-st.subheader("실시간 전력")
+# 최근 데이터 표시
+recent_data = get_recent_data(data)
+st.subheader("최근 데이터")
 if data:
-    latest_data = data[-1]
-    st.write(f"시간: {latest_data[0]}, 전력량: {latest_data[1]} [W]")
+    for timestamp, value in recent_data:
+        st.write(f"시간: {timestamp}, 전력량: {value} [W]")
 
-st.subheader("전체 데이터")
-PAGE_SIZE = 10
-num_pages = len(data) // PAGE_SIZE + 1
-page_number = st.number_input("페이지 번호", min_value=1, max_value=num_pages, value=1)
-start_index = (page_number - 1) * PAGE_SIZE
-end_index = min(len(data), page_number * PAGE_SIZE)
-with st.expander("전체 데이터 보기"):
-    with st.container():
-        st.write(f"페이지: {page_number} / {num_pages}, 데이터 범위: {start_index + 1} - {end_index}")
-        for idx, (timestamp, value) in enumerate(data[start_index:end_index], start=start_index + 1):
-            st.write(f"{idx}. 시간: {timestamp}, 전력량: {value} [W]")
-
+# 통계 정보
 st.subheader("통계 정보")
 if data:
     values = [item[1] for item in data]
